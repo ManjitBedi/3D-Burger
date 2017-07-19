c04a237b0117c925af3ea698cd6060e8086c9e3d
ManjitBedi <work@manjitbedi.com>
2017-07-19 12:22:34 -0700

    Getting started

---
+++ 3D Burger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3D Burger
+//
+//  Created by Manjit Bedi on 2017-07-19.
+//  Copyright © 2017 noorg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
